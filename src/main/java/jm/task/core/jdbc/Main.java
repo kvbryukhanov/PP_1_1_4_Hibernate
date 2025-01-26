788a51081b005bbb5e2cef313c861324760a6ec7
konstantin <kvbryukhanov@gmail.com>
2025-01-26 18:46:41 +0300

    2025-01-26 Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,8 +5,6 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 import java.security.Provider;
 
